9c823b9a92d9429abcd58e3b2bbbd46b3475a6fd
yuwang <361467298@qq.com>
2018-01-18 18:05:10 +0800

    no message

--- TiTV/ViewController.swift
+++ TiTV/ViewController.swift
@@ -7,7 +7,8 @@
 //
 
 import UIKit
-
+import AVKit
+//import MediaPlayer
 class ViewController: UIViewController, UIWebViewDelegate, UITextFieldDelegate {
 
     
@@ -23,10 +24,14 @@ class ViewController: UIViewController, UIWebViewDelegate, UITextFieldDelegate {
         //添加代理委托
         textField.delegate = self
         webview.delegate = self
-        
+        webview.allowsInlineMediaPlayback = true
         
         
         btnVip.addTarget(self, action:#selector(btnVipTapped), for:.touchUpInside)
+
+
+//        NotificationCenter.default.addObserver(self, selector:  #selector(notificationAction), name: NSNotification.Name.UIWindowDidBecomeHidden, object: nil);
+
     }
     @objc func btnVipTapped(){
         
@@ -43,9 +48,12 @@ class ViewController: UIViewController, UIWebViewDelegate, UITextFieldDelegate {
             //
             aUrl = String(format:aUrl,curUrl!)
         }
+        else
+        {
+            aUrl = "http://www.hifiwu.com/index.php"
+        }
         
-        
-        print(aUrl as Any)
+//        print(aUrl as Any)
         loadUrl(url: aUrl,web: webview);
     }
     @IBAction func goback(_ sender: UIButton) {
@@ -79,9 +87,94 @@ class ViewController: UIViewController, UIWebViewDelegate, UITextFieldDelegate {
         if (request.url?.absoluteString.contains("about:blank"))! {
             return false
         }
+
+//        if ((request.url?.scheme) != nil) {
+//            print(request.url?.scheme);//在这里可以获得事件
+//        }
+
+        if (request.url?.absoluteString.contains("share"))! {
+            let data = NSData.init(contentsOf: URL.init(string: (request.url?.absoluteString)!)!)
+            print(NSString.init(data: data! as Data, encoding: 0))
+        }
+
         let canReturn = request.url?.absoluteString .contains("http://f.qcwzx.net.cn")
         print(request.url?.absoluteString)
+//        print(request.httpBody)
+//        print(request.allHTTPHeaderFields)
+//        print(request.httpShouldUsePipelining)
+//        print(request.networkServiceType)
         return !canReturn!
+    }
+
+    @objc private func notificationAction(noti: Notification) {
+        /// 获取键盘的位置/高度/时间间隔...
+//        for object in noti.object.map({ (object) -> Array<Any> in
+//            return [object]
+//        })! {
+//            print((object as AnyObject).classForCoder)
+//        }
+//        print(noti.object)
+//        print(self.view.subviews)
+//        for view in self.view.subviews {
+//            print(view.classForCoder)
+//        }
+        
+        let keyWindow = UIApplication.shared.windows
+//        print(keyWindow)
+//        print(keyWindow)
+        for keyWindowView in  keyWindow {
+            if keyWindowView .isKind(of: UIWindow.classForCoder()){
+                let subViews = keyWindowView.rootViewController?.view.subviews
+                viewClass(viewClass: keyWindowView.classForCoder)
+                if (subViews != nil) {
+                    for subSubView in subViews! {
+                        if subSubView .isKind(of: NSClassFromString("AVPlayerView")!) {
+
+                            let playerView = subSubView
+                            let contentView = playerView.value(forKey: "_contentView") as! UIView
+//                            viewClass(viewClass: contentView.classForCoder)
+
+
+
+
+                            let delegate = contentView.value(forKey: "_delegate") as! NSObject
+
+                            let playerVC = delegate.value(forKey: "_playerViewController") as! AVPlayerViewController
+//                            let player = playerVC.value(forKey: "_player") as! AVPlayer
+//                            print(playerVC)
+//Builtin
+                        }
+                    }
+                }
+            }
+        }
+
+
+//        AVAudioSession
+//AVSystemController_ActiveAudioRouteDidChangeNotification
+//
+
+//        for view in webview.subviews {
+//            if view .isKind(of: NSClassFromString("_UIWebViewScrollView")!) {
+//                for subView in view.subviews {
+//                    if subView .isKind(of: NSClassFromString("UIWebBrowserView")!) {
+//                        print(subView.subviews)
+//                        print(subView.layer.sublayers)
+//                    }
+//                }
+//            }
+//        }
+    }
+
+    func viewClass (viewClass :AnyClass) {
+        var outCount:UInt32 = 0
+        let ivars = class_copyIvarList(viewClass, &outCount)
+        for i in 0..<outCount {
+            let ivar = ivars?[Int(i)];
+            let key = NSString.init(utf8String:ivar_getName(ivar))
+            print(key)
+        }
+        free(ivars);
     }
 }
 
